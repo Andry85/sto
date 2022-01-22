51a09d8c6c5f7785e72b24e2def61eb1d6e5883e
Andii Shapoval <andrii.shapoval@smile-ukraine.com>
2022-01-22 23:30:54 +0200

    added posts

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const dotenv = require('dotenv');
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
 
 dotenv.config();
 app.use(express.json());
@@ -23,6 +24,7 @@ mongoose.connect(process.env.MONGO_URL)
 
 app.use('/api/auth', authRoute);
 app.use('/api/users', userRoute);
+app.use('/api/posts', postRoute);
 
 
 
