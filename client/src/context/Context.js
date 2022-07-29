c67d78a83be0e7fb01ea7e245029d6fe1a4f46b9
Andry85 <shapovala@ukr.net>
2022-07-29 13:35:05 +0300

    fixed

--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -9,7 +9,7 @@ export const GoogleContextProvider = ({children}) => {
 
     useEffect(() => {
         const getUser = async()=> {
-            fetch(`${process.env.HOST}/auth/login/success`, {
+            fetch(`${process.env.REACT_APP_DOMAIN}/auth/login/success`, {
                 method: "GET",
                 credentials: 'include',
                 headers: {
