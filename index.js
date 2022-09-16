4186c2f707a93a75ed5e5ce7f49fb36464ab9ffb
Andry85 <shapovala@ukr.net>
2022-09-16 12:51:44 +0300

    fixed cors

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.use(cookieSession(
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors({
-    origin: CLIENT_URL,
+    origin: "*",
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
 }));
